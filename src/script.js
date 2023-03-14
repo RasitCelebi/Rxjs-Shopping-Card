b07b81e006135e97f3af50a24ea0b4dbd46885d0
RasitCelebi <rasit_61@hotmail.com>
2023-03-14 16:04:09 +0300

    I added rxjs as cdn version

--- src/script.js
+++ src/script.js
@@ -1,3 +1,11 @@
 import { products } from "./productList.js";
+const { range, filter, map } = rxjs;
+
+range(1, 200)
+  .pipe(
+    filter((x) => x % 2 === 1),
+    map((x) => x + x)
+  )
+  .subscribe((x) => console.log(x));
 
 console.log(products);
