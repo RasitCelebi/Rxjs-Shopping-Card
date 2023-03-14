3322d65946709483a671c338a4128ac309133892
RasitCelebi <rasit_61@hotmail.com>
2023-03-14 15:58:22 +0300

    i imported other js file and still tailwind is running

---
+++ src/productList.js
@@ -0,0 +1,71 @@
+export const products = [
+    {
+        id: 0,
+        image: 'https://cdn.shopify.com/s/files/1/0557/8414/5067/products/0a6753bc44bc636a_1024x1024@2x.jpg?v=1622798607',
+        title: 'GİYİLEBİLİR DÖVME TATTOO SLEEVES',
+        price: 100
+    },
+
+    {
+        id: 1,
+        image: 'https://cdn.shopify.com/s/files/1/0557/8414/5067/products/a74907e725-2904_1024x1024@2x.jpg?v=1622799292',
+        title: '6 GÖZLÜ PRATİK ÇANTA ORGANİZER',
+        price: 200
+    },
+
+    {
+        id: 2,
+        image: 'https://cdn.shopify.com/s/files/1/0557/8414/5067/products/d197dd1872aaf505_1024x1024@2x.jpg?v=1643968718',
+        title: 'GEYİK TASARIMLI TAKI, MÜCEVHER STANDI',
+        price: 300
+    },
+
+    {
+        id: 3,
+        image: 'https://cdn.shopify.com/s/files/1/0557/8414/5067/products/c0ff25fffc46c767_1024x1024@2x.jpg?v=1622799071',
+        title: 'AKILLI TELEFONLARLA UYUMLU SERİNLETİCİ FAN',
+        price: 400
+    },
+
+    {
+        id: 4,
+        image: 'https://cdn.shopify.com/s/files/1/0557/8414/5067/products/58be763fba-415_1024x1024@2x.jpg?v=1625232265',
+        title: 'IŞIKLI KİTAP AYRACI',
+        price: 500
+    },
+
+    {
+        id: 5,
+        image: 'https://cdn.shopify.com/s/files/1/0557/8414/5067/products/79a56a7dbfe0022a_1024x1024@2x.jpg?v=1622798894',
+        title: 'DELİK KOVA GÖRÜNÜMLÜ KALEMLİK',
+        price: 600
+    },
+
+    {
+        id: 6,
+        image: 'https://cdn.shopify.com/s/files/1/0557/8414/5067/products/4295fcb7ff-1729_1024x1024@2x.jpg?v=1622798630',
+        title: 'BALANS BİLYE',
+        price: 700
+    },
+
+    {
+        id: 7,
+        image: 'https://cdn.shopify.com/s/files/1/0557/8414/5067/products/860d5e617048eae0_1024x1024@2x.jpg?v=1618823650',
+        title: 'LED IŞIKLI KİTAP OKUMA GÖZLÜĞÜ',
+        price: 800
+    },
+
+    {
+        id: 8,
+        image: 'https://cdn.shopify.com/s/files/1/0557/8414/5067/products/e86b447a7eb55931_1024x1024@2x.jpg?v=1622799574',
+        title: 'DOĞAL KÜTÜK BARDAK ALTLIĞI (6 ADET)',
+        price: 900
+    },
+
+    {
+        id: 9,
+        image: 'https://cdn.shopify.com/s/files/1/0557/8414/5067/products/41e56e2925a94e30_1024x1024@2x.jpg?v=1622799129',
+        title: 'EVCİL HAYVAN TÜY TOPLAMA ELDİVENİ',
+        price: 1000
+    }
+];
---
+++ src/script.js
@@ -0,0 +1,3 @@
+import { products } from "./productList.js";
+
+console.log(products);
